553d44d807d5f154d84e35fe6f7380ab5a6ed463
Jim Lin <jimlintw922@gmail.com>
2018-03-02 17:12:16 +0800

    mod main.go

--- license-client/main.go
+++ license-client/main.go
@@ -1,8 +1,24 @@
 package main
 
+import "syscall"
 import "os"
+import "os/exec"
 import "fmt"
 
 func main() {
 	fmt.Println("LICENSE_SERVER: ", os.Getenv("LICENSE_SERVER"))
+
+    binary, lookErr := exec.LookPath("curl")
+    if lookErr != nil {
+        panic(lookErr)
+    }
+
+    args := []string{"curl", "--cert", "/tmp/pki/client.cert.pem", "--key", "/tmp/pki/client.key.pem", "--cacert", "/tmp/pki/ca.cert.pem", os.Getenv("LICENSE_SERVER")}
+
+    env := os.Environ()
+
+    execErr := syscall.Exec(binary, args, env)
+    if execErr != nil {
+        panic(execErr)
+    }
 }
