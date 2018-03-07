bbb146dcd9101bfd772c9a44b6751e20cf71e857
Jim Lin <jimlintw922@gmail.com>
2018-03-07 09:43:28 +0800

    mod test.sh to add local dev license-server option

--- license-client/test.sh
+++ license-client/test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-export LICENSE_SERVER=https://192.168.240.56.xip.io/wsgi
-./license-client
+# use local dev license-server
+#docker run -dit -h localhost.localdomain --name httpd-2.4 -p 8080:80 -p 8443:443 -v "$PWD/data":/usr/local/apache2/htdocs/ my-httpd-2.4
+export LICENSE_SERVER=https://localhost.localdomain:8443/wsgi
+go run main.go
+
+# use remote dev license-server
+#export LICENSE_SERVER=https://192.168.240.56.xip.io/wsgi
+#go run main.go
