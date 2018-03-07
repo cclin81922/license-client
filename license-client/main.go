aa4d27bcf45900ec7abc0eb160b9609fc86621a5
Jim Lin <jimlintw922@gmail.com>
2018-03-07 13:43:07 +0800

    mod main.go; add .vscode/launch.json

--- license-client/main.go
+++ license-client/main.go
@@ -1,15 +1,22 @@
 package main
 
 import "os"
-
+import "os/exec"
 import "fmt"
 
 func main() {
-	fmt.Println("LICENSE_SERVER: ", os.Getenv("LICENSE_SERVER"))
+	fmt.Println("DEBUG | LICENSE_SERVER: ", os.Getenv("LICENSE_SERVER"))
+	licenseServer := os.Getenv("LICENSE_SERVER")
 
 	// command 1
 	// curl --cert ./data/pki/client.cert.pem --key ./data/pki/client.key.pem --cacert ./data/pki/ca.cert.pem ${LICENSE_SERVER}
 	// []string{"curl", "--cert", "./data/pki/client.cert.pem", "--key", "./data/pki/client.key.pem", "--cacert", "./data/pki/ca.cert.pem", os.Getenv("LICENSE_SERVER")}
+	curlCmd := exec.Command("curl", "--cert", "./data/pki/client.cert.pem", "--key", "./data/pki/client.key.pem", "--cacert", "./data/pki/ca.cert.pem", licenseServer)
+	curlOut, curlErr := curlCmd.Output()
+	if curlErr != nil {
+		panic(curlErr)
+	}
+	fmt.Println("DEBUG | curlOut: ", string(curlOut))
 
 	// command 2
 	// dd if=/opt/src.des3 | openssl des3 -d -k "\$PASS" | tar zxf - -C /opt
